b6d9101110740b2b329ed1bc859d9be9458c416a
user <taraassov03@gmail.com>
2023-07-05 13:22:30 +0500

    item

--- src/item.py
+++ src/item.py
@@ -1,3 +1,5 @@
+import csv
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -6,9 +8,6 @@ class Item:
     all = []
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
-        self.name = name
-        self.prise = price
-        self.quantity = quantity
         """
         Создание экземпляра класса item.
 
@@ -16,7 +15,28 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.all.append(self)
+
+        self.name = name
+        self.prise = price
+        self.quantity = quantity
+        Item.all.append(self)
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity})"
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        if len(value) > 10:
+            self.__name = value[:10]
+        else:
+            self.__name = value
 
     def calculate_total_price(self) -> float:
         """
@@ -31,3 +51,24 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
+
+    @classmethod
+    def instantiate_from_csv(cls, filename='../src/items.csv'):
+        """
+        Инициализирует экземпляры класса Item данными из файла src/items.csv
+        """
+        with open(filename, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                # print(row)
+                name = row['name']
+                price = cls.string_to_number(row['price'])
+                quantity = int(row['quantity'])
+                cls(name, price, quantity)
+
+    @staticmethod
+    def string_to_number(string: str) -> float:
+        """
+        Возвращает число из числа-строки.
+        """
+        return int(float(string))
