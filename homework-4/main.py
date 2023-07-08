c134ce7c6130c6c53f77cd41092680d66cf320fc
user <taraassov03@gmail.com>
2023-07-08 13:28:19 +0500

    main_.py

--- homework-4/main.py
+++ homework-4/main.py
@@ -7,11 +7,17 @@ if __name__ == '__main__':
     phone1 = Phone("iPhone 14", 120_000, 5, 2)
     assert str(phone1) == 'iPhone 14'
     assert repr(phone1) == "Phone('iPhone 14', 120000, 5, 2)"
+    #print(str(phone1))
+    #print(repr(phone1))
     assert phone1.number_of_sim == 2
 
     item1 = Item("Смартфон", 10000, 20)
     assert item1 + phone1 == 25
     assert phone1 + phone1 == 10
+    #print(item1 + phone1)
+    #print(phone1 + phone1)
 
     phone1.number_of_sim = 0
     # ValueError: Количество физических SIM-карт должно быть целым числом больше нуля.
+
+#phone3 = Phone("sony", 8000, 5, 0)
