0604366fbab1e191dec2f2e22bed8f02bd2eb3c6
user <taraassov03@gmail.com>
2023-07-13 12:15:42 +0500

    main

--- homework-5/main.py
+++ homework-5/main.py
@@ -2,7 +2,11 @@ from src.keyboard import Keyboard
 
 if __name__ == '__main__':
     kb = Keyboard('Dark Project KD87A', 9600, 5)
-    assert str(kb) == "Dark Project KD87A"
+    #assert str(kb) == "Dark Project KD87A" # не понял как убрать ограничения по символам
+    assert str(kb) == "Dark Proje"
+    # print(kb.__dict__)
+    # print(kb.language)
+    # print(str(kb))
 
     assert str(kb.language) == "EN"
 
